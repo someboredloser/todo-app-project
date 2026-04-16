fa02f64c01c46e507a81efcf0992057e6108d242
Nikita <amnezia703@gmail.com>
2026-04-16 15:33:00 +0200

    feat: add repositories and minor fixes

--- models/task.py
+++ models/task.py
@@ -3,5 +3,11 @@ from uuid import uuid4
 from app.database.base import Base
 from sqlalchemy.orm import Mapped, mapped_column
 
-class User(Base):
-    id: Mapped[str] = mapped_column(primary_key=True, default=lambda: str(uuid4()))
+class TaskORM(Base):
+    __tablename__ = "tasks"
+    id: Mapped[str] = mapped_column(
+        primary_key=True, 
+        default=lambda: str(uuid4())
+    )
+    title: Mapped[str]
+    completed: Mapped[bool] = mapped_column(default=False)
